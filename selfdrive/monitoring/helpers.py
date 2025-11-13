61ec93df45dbc8ae5c1d40e7421e1e723c478fd0
RevolutionAire <120480298+RevolutionAire@users.noreply.github.com>
2025-11-13 08:17:14 -0500

    Increase passive wheeltouch timeout

--- selfdrive/monitoring/helpers.py
+++ selfdrive/monitoring/helpers.py
@@ -22,7 +22,7 @@ class DRIVER_MONITOR_SETTINGS:
   def __init__(self):
     self._DT_DMON = DT_DMON
     # ref (page15-16): https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:42018X1947&rid=2
-    self._AWARENESS_TIME = 300. # passive wheeltouch total timeout
+    self._AWARENESS_TIME = 3000. # passive wheeltouch total timeout
     self._AWARENESS_PRE_TIME_TILL_TERMINAL = 1500.
     self._AWARENESS_PROMPT_TIME_TILL_TERMINAL = 600.
     self._DISTRACTED_TIME = 1100. # active monitoring total timeout
